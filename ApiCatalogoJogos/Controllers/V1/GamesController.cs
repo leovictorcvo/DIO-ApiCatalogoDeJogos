2d26c69f6df72b9be59ce6c608513e87f363e5e0
Leonardo Victor Soares Medeiros <leovictorcvo@gmail.com>
2021-05-08 08:38:35 -0300

    Acrescentado informações para o Swagger

--- ApiCatalogoJogos/Controllers/V1/GamesController.cs
+++ ApiCatalogoJogos/Controllers/V1/GamesController.cs
@@ -31,8 +31,9 @@ namespace ApiCatalogoJogos.Controllers.V1
         /// </remarks>
         /// <param name="pagina">Número da página desejada</param>
         /// <param name="quantidade">Quantidade de jogos para serem exibidos em cada página</param>
+        /// <returns>Retorna status Ok e a lista dos jogos cadastrados</returns>
         /// <response code="200">Retorna a lista de jogos</response>
-        /// <response code="204">Não há jogos cadastrados</response>
+        /// <response code="204">Não há jogos cadastrados</response>        
         [HttpGet]
         public async Task<ActionResult<List<GameViewModel>>> Get([FromQuery, Range(1, int.MaxValue)] int pagina = 1, [FromQuery, Range(1, 50)] int quantidade = 5)
         {
@@ -45,6 +46,13 @@ namespace ApiCatalogoJogos.Controllers.V1
             return Ok(games);
         }
 
+        /// <summary>
+        /// Retorna um jogo baseado no id fornecido
+        /// </summary>
+        /// <param name="id">Identificador do jogo a ser pesquisado</param>
+        /// <returns>Retorna status Ok e dados do jogo</returns>
+        /// <response code="200">Retorna o jogo encontrado</response>
+        /// <response code="204">Não há jogo cadastrado com o Id fornecido</response>        
         [HttpGet("{id:Guid}")]
         public async Task<ActionResult<GameViewModel>> Get(Guid id) 
         {
@@ -56,6 +64,13 @@ namespace ApiCatalogoJogos.Controllers.V1
             return Ok(game);
         }
 
+        /// <summary>
+        /// Cadastra um novo jogo
+        /// </summary>
+        /// <param name="game">Dados do jogo para cadastrar</param>
+        /// <returns>Retorna status Created e dados do jogo</returns>
+        /// <response code="201">Jogo criado com sucesso</response>
+        /// <response code="422">Não foi possível cadastrar o jogo com os dados fornecidos</response>        
         [HttpPost]
         public async Task<ActionResult<GameViewModel>> Create([FromBody] GameInputModel game)
         {
@@ -70,6 +85,14 @@ namespace ApiCatalogoJogos.Controllers.V1
             }
         }
 
+        /// <summary>
+        /// Atualiza os dados de um jogo já cadastrado
+        /// </summary>
+        /// <param name="id">Identificador do jogo a ser alterado</param>
+        /// <param name="game">Novos dados do jogo</param>
+        /// <returns>Retorna status Ok</returns>
+        /// <response code="200">Jogo alterado com sucesso</response>
+        /// <response code="404">Não foi encontrado o jogo com identificador fornecido</response>        
         [HttpPut("{id:Guid}")]
         public async Task<ActionResult> Update([FromRoute] Guid id, [FromBody] GameInputModel game)
         {
@@ -84,6 +107,13 @@ namespace ApiCatalogoJogos.Controllers.V1
             }
         }
 
+        /// <summary>
+        /// Altera o valor de venda de um jogo
+        /// </summary>
+        /// <param name="id">Identificador do jogo a ser alterado</param>
+        /// <param name="price">Novo valor de venda</param>
+        /// <response code="200">Jogo alterado com sucesso</response>
+        /// <response code="404">Não foi encontrado o jogo com identificador fornecido</response>        
         [HttpPatch("{id:Guid}/preco/{price:double}")]
         public async Task<ActionResult> UpdatePrice([FromRoute] Guid id, [FromRoute] double price)
         {
@@ -98,6 +128,12 @@ namespace ApiCatalogoJogos.Controllers.V1
             }
         }
 
+        /// <summary>
+        /// Exclui um jogo cadastrado
+        /// </summary>
+        /// <param name="id">Identificador do jogo a ser excluído</param>
+        /// <response code="200">Jogo excluído com sucesso</response>
+        /// <response code="404">Não foi encontrado o jogo com identificador fornecido</response>        
         [HttpDelete("{id:Guid}")]
         public async Task<ActionResult> Delete([FromRoute] Guid id)
         {
